b6105bbc5176e1d3f176960b61533a861a68c521
Charly Cimino <carlosecimino@gmail.com>
2023-05-09 23:31:17 -0300

    Cardinalidad y nombre regex

--- js/classes/Relacion.js
+++ js/classes/Relacion.js
@@ -8,10 +8,13 @@ class Relacion {
 
         console.log(relItem);
 
-        let [tipoFlecha, cardinalidad, nombreRelacion] = relItem;
+        let [tipoFlecha, cardinalidad = "1", nombreRelacion = ""] = relItem;
 
         let match = Relacion.getTipoFlechaRegex().exec(tipoFlecha);
         if (!match) throw new Error(`No se pudo parsear la relación dada por el tipo de flecha: '${tipoFlecha}'\n${REVISAR_SINTAXIS_LETINO}`);
+
+        match = Relacion.getCardinalidadRegex().exec(cardinalidad);
+        if (!match) throw new Error(`No se pudo parsear la cardinalidad '${cardinalidad}' de la relación dada por el tipo de flecha '${tipoFlecha}'\n${REVISAR_SINTAXIS_LETINO}`);
 
         /*if (tipoFlecha.includes('>>') || tipoFlecha.includes('<<')) {
             return RelacionHerencia.parse(tipoFlecha, coord);
@@ -25,7 +28,23 @@ class Relacion {
 
     static getTipoFlechaRegex() {
         return createRegex([
-            /^lt=(<(?:(?:\-(?:>{4,5})?)|(?:\.+)|(?:<[\-\.])))$/
+            /^lt=(<(?:(?:\-(?:>{4,5})?)|(?:\.+)|(?:<[\-\.])))\s*$/
+        ]);
+    }
+
+    static getCardinalidadRegex() {
+        return createRegex([
+            /^(?:m1=\s*)?((?:[1*n])|(?:[0-1]\.\.[1*n])|(?:\d+))\s*$/
+        ]);
+    }
+
+    static getNombreRelacionRegex() {
+        return createRegex([
+            /^/,                                            // Inicio de línea
+            /([-+#]?)\s*/,                                  // Visibilidad (opcional)
+            /([a-zA-Z](?:[a-zA-Z0-9_]*[a-zA-Z0-9])?)\s*/,   // Identificador
+            /$/,                                            // Fin de línea
         ]);
     }
 }
+
